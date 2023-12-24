7dd3a6a3d518ae04060ca046be072441c5b9ddfe
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2023-12-24 23:47:10 +0900

    edit test.sh

--- test.sh
+++ test.sh
@@ -4,3 +4,4 @@
 repl_lang=$(gum filter $(curl -s -X GET --url "https://glot.io/api/run" | jq -r '.[] | .name'))
 read -p "$repl_lang > "
 echo "You typed $REPLY"
+echo "https://glot.io/api/run/$repl_lang/latest"
