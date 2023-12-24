377479bb360e0d7201f6d049875eec3584a2df72
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2023-12-24 23:45:13 +0900

    edit test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 # The input to `read` is set into $REPLY variable
-prompt_lang=$(gum filter $(curl -s -X GET --url "https://glot.io/api/run" | jq -r '.[] | .name'))
-read -p "$prompt_lang > "
+repl_lang=$(gum filter $(curl -s -X GET --url "https://glot.io/api/run" | jq -r '.[] | .name'))
+read -p "$repl_lang > "
 echo "You typed $REPLY"
