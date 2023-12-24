58162a0b8778b7661cffd6b07a0a017a6db91bde
AlpineYahoo <101258543+alpineyahoo@users.noreply.github.com>
2023-12-24 23:01:36 +0900

    edit example.sh

--- example.sh
+++ example.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cd "$(dirname "$(readlink -f "$0")")"
 source config
-curl -s -X POST -H $TOKEN -H $HEADER -d '{"files": [{"name": "main.py", "content": "print(42)"}]}' \
+curl -s -X POST -H "$TOKEN" -H "$HEADER" -d '{"files": [{"name": "main.py", "content": "print(42)"}]}' \
      --url 'https://glot.io/api/run/python/latest' | /usr/local/bin/jq
 
 # zsh example.sh
